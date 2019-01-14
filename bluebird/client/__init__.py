934ff1847c13d433381b12d0c9d8c5b2c5c6f048
Ruairidh MacLeod <5160559+rkm@users.noreply.github.com>
2019-01-14 16:32:12 +0000

    Fix import ordering

--- bluebird/client/__init__.py
+++ bluebird/client/__init__.py
@@ -5,11 +5,11 @@ Contains objects for communicating with the client simulation
 import os
 import sys
 
-from .client import ApiClient
-
 # Import BlueSky from the git submodule
 sys.path.append(
 				os.path.join(os.path.dirname(os.path.realpath(__file__)), os.path.abspath('./bluesky')))
+
+from .client import ApiClient
 
 # Global reference to the client
 CLIENT_SIM = ApiClient()
