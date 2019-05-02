a7fdc794830d33eedaf5efa40c5d48d242535abb
Ruairidh MacLeod <5160559+rkm@users.noreply.github.com>
2019-05-02 15:15:57 +0100

    Add "close_ep" option to close and reset sim when requesting the log

--- bluebird/api/resources/eplog.py
+++ bluebird/api/resources/eplog.py
@@ -5,9 +5,13 @@ Provides logic for the 'eplog' (episode log file) API endpoint
 import os
 
 from flask import jsonify
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 
+import bluebird.client as bb_client
 import bluebird.logging as bb_logging
+
+PARSER = reqparse.RequestParser()
+PARSER.add_argument('close_ep', type=bool, location='json', required=False)
 
 
 class EpLog(Resource):
@@ -22,6 +26,8 @@ class EpLog(Resource):
 		:return: :class:`~flask.Response`
 		"""
 
+		parsed = PARSER.parse_args(strict=True)
+
 		ep_file_path = bb_logging.EP_FILE
 
 		if not ep_file_path:
@@ -29,8 +35,15 @@ class EpLog(Resource):
 			resp.status_code = 404
 			return resp
 
+		if parsed['close_ep']:
+			err = bb_client.CLIENT_SIM.reset_sim()
+			if err:
+				resp = jsonify(f'Could not reset simulation: {err}')
+				resp.status_code = 500
+				return resp
+
 		full_ep_file = os.path.join(os.getcwd(), ep_file_path)
-		lines = tuple(open(full_ep_file, 'r'))
+		lines = tuple(open(full_ep_file))
 
 		resp = jsonify({'cur_ep_id': bb_logging.EP_ID, 'cur_ep_file': full_ep_file, 'lines': lines})
 		resp.status_code = 200
