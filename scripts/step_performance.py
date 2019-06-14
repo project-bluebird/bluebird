a7ea8a2ab35a025c3ef20f37bc4ca4216e217795
Ruairidh MacLeod <5160559+rkm@users.noreply.github.com>
2019-06-14 14:42:34 +0100

    Test performance for different multiplier speeds

--- scripts/step_performance.py
+++ scripts/step_performance.py
@@ -3,22 +3,26 @@ Measures the performance of the STEP endpoint
 """
 
 import argparse
-import requests
 import time
+
+import requests
+
 
 API_URL_BASE = f'http://localhost:5001/api/v1'
 
 
-def main():
-	parser = argparse.ArgumentParser()
-	parser.add_argument('--max_steps', type=int, default=5)
-	args = parser.parse_args()
-	max_steps = args.max_steps
+def measure_step(max_steps, multiplier):
+	"""
+	Measure the performance of STEP
+	:param max_steps:
+	:param multiplier:
+	:return:
+	"""
 
 	resp = requests.post(f'{API_URL_BASE}/ic', json={'filename': 'TEST.scn'})
 	assert resp.status_code == 200, 'Expected the scenario to be loaded'
 
-	resp = requests.post(f'{API_URL_BASE}/dtmult', json={'multiplier': 5})
+	resp = requests.post(f'{API_URL_BASE}/dtmult', json={'multiplier': multiplier})
 	assert resp.status_code == 200, 'Expected DTMULT to be set'
 
 	times = []
@@ -43,12 +47,26 @@ def main():
 	except KeyboardInterrupt:
 		print('Cancelled')
 
-	if not len(times):
+	if not times:
 		print('No data collected')
 		return
 
 	print(f'times: {times}')
-	print(f'Mean step time {sum(times)/float(len(times)):.2f}s')
+	print(f'Mean step time {sum(times)/float(len(times)):.2f}s (dtmult={multiplier})')
+
+
+def main():
+	"""
+	Main
+	:return:
+	"""
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--max_steps', type=int, default=5)
+	args = parser.parse_args()
+
+	for mult in range(6):
+		measure_step(args.max_steps, 10 + mult * 10)
 
 
 if __name__ == '__main__':
