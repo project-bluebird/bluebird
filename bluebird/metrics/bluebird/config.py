dc86d8ca3dd884a408dc58b52390cfd057776bee
Ruairidh MacLeod <5160559+rkm@users.noreply.github.com>
2019-08-13 13:13:35 +0100

    Refactor metrics config and tests

--- bluebird/metrics/bluebird/config.py
+++ bluebird/metrics/bluebird/config.py
@@ -4,12 +4,12 @@ Configuration for BlueBird's built-in evaluation metrics
 
 # TODO Proper support for units
 
+LOS_SCORE = -1  # Score below the minimum separation
+
 # Vertical separation (ft)
-VERT_LOS_SCORE = -1  # Score below the minimum separation
 VERT_MIN_DIST = 1000
-VERT_WARN_DIST = 2000
+VERT_WARN_DIST = 2 * VERT_MIN_DIST
 
 # Horizontal separation (nm)
-HOR_LOS_SCORE = -1  # Score below the minimum separation
 HOR_MIN_DIST = 5
-HOR_WARN_DIST = 10
+HOR_WARN_DIST = 2 * HOR_MIN_DIST
--- bluebird/metrics/bluebird/metrics.py
+++ bluebird/metrics/bluebird/metrics.py
@@ -1,5 +1,26 @@
 """
-(Very) basic evaluation metrics
+Basic aircraft separation metric. Specification is:
+
+Let's denote:
+
+- d_h := absolute horizontal distance between the two aircraft (either geodesic or great circle
+separation will do) measured in nautical miles (nm)
+- d_v := absolute vertical separation between the two aircraft measured in feet (ft)
+
+The simple metric *m* I have in mind is the function of d_h and d_v defined by:
+
+- m(d_h, d_v) = 0, if d_h >= C_h (for any d_v)
+- m(d_h, d_v) = 0, if d_v >= C_v (for any d_h)
+- m(d_h, d_v) = -1, if d_h < c_h and d_v < c_v (loss of separation)
+- m(d_h, d_v) = max{ (d_h - c_h)/(C_h - c_h) - 1, (d_v - c_v)/(C_v - c_v) - 1 }, otherwise
+
+where:
+
+- The c_h (horizontal) and c_v (vertical) thresholds are part of the definition of "loss of
+separation", i.e. they're given constants: c_h = 5 nm, c_v = 1000 ft
+- The C_h and C_v thresholds are arbitrary parameters (except for the requirement that C_h > c_h
+and C_v > c_v), so the function should take them as arguments. Default values could be double the
+corresponding lower thresholds, that is: C_h = 10 nm, C_v = 2000 ft
 """
 
 import numpy as np
@@ -15,6 +36,7 @@ _ONE_NM = 1852  # Meters
 
 _SCALE_METRES_TO_FEET = 3.280839895
 
+
 def _get_pos(acid):
 	assert isinstance(acid, str), 'Expected the input to be a string'
 	assert is_acid(acid), 'Expected the input to be a valid ACID'
@@ -22,7 +44,7 @@ def _get_pos(acid):
 	return AC_DATA.get(acid)[acid]
 
 
-def vertical_separation(acid1, acid2):
+def _vertical_separation(acid1, acid2):
 	"""
 	Basic vertical separation metric
 	:param acid1:
@@ -37,17 +59,17 @@ def vertical_separation(acid1, acid2):
 	vertical_sep_ft = vertical_sep_metres * _SCALE_METRES_TO_FEET
 
 	if vertical_sep_ft < cfg.VERT_MIN_DIST:
-		return cfg.VERT_LOS_SCORE
+		return cfg.LOS_SCORE
 
 	if vertical_sep_ft < cfg.VERT_WARN_DIST:
 		# Linear score between the minimum and warning distances
 		return np.interp(vertical_sep_ft,
-		                 [cfg.VERT_MIN_DIST, cfg.VERT_WARN_DIST], [cfg.VERT_LOS_SCORE, 0])
+		                 [cfg.VERT_MIN_DIST, cfg.VERT_WARN_DIST], [cfg.LOS_SCORE, 0])
 
 	return 0
 
 
-def horizontal_separation(acid1, acid2):
+def _horizontal_separation(acid1, acid2):
 	"""
 	Basic horizontal separation metric
 	:param acid1:
@@ -62,25 +84,26 @@ def horizontal_separation(acid1, acid2):
 	horizontal_sep_nm = round(horizontal_sep_m / _ONE_NM)
 
 	if horizontal_sep_nm < cfg.HOR_MIN_DIST:
-		return round(cfg.HOR_LOS_SCORE, 1)
+		return round(cfg.LOS_SCORE, 1)
 
 	if horizontal_sep_nm < cfg.HOR_WARN_DIST:
 		# Linear score between the minimum and warning distances
 		return round(np.interp(horizontal_sep_nm,
-		                       [cfg.HOR_MIN_DIST, cfg.HOR_WARN_DIST], [cfg.HOR_LOS_SCORE, 0]), 1)
+		                       [cfg.HOR_MIN_DIST, cfg.HOR_WARN_DIST], [cfg.LOS_SCORE, 0]), 1)
 
 	return 0
 
 
-def separation_metric(acid1, acid2):
+def aircraft_separation(acid1, acid2):
 	"""
 	Combined score based on horizontal and vertical separation.
 	:param acid1:
 	:param acid2:
 	:return:
 	"""
-	horizontal_sep = horizontal_separation(acid1, acid2)
-	vertical_sep = vertical_separation(acid1, acid2)
+
+	horizontal_sep = _horizontal_separation(acid1, acid2)
+	vertical_sep = _vertical_separation(acid1, acid2)
 
 	if horizontal_sep == 0 and vertical_sep == 0:
 		return 0
