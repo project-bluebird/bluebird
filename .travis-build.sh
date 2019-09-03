102ffd06805738dbe10c909b6e155afd53cb170f
Tarek Allam <t.allam.jr@gmail.com>
2019-09-03 11:34:35 +0100

    Fix credential warning message
    Warning:
    
        echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME"
        --password-stdin
        WARNING! Your password will be stored unencrypted in
        /home/travis/.docker/config.json.
    
    REF:
    
        https://github.com/travis-ci/travis-ci/issues/9495
        https://github.com/osism/docker-template/commit/70db3f9f555bc35659dfc7028f0d5331e745460b

--- .travis-build.sh
+++ .travis-build.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 set -ev
 cd $BLUEBIRD_HOME
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt update
+sudo apt -y install docker-ce
+echo '{ "experimental": true }' | sudo tee /etc/docker/daemon.json
+sudo service docker restart
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+cat /home/travis/.docker/config.json
 # If CI on master branch then tag with latest
 if [ $TRAVIS_BRANCH = "master" ]; then
     docker build --tag=bluebird .
@@ -11,7 +17,8 @@ fi
 # If CI on release branch branch then tag with that release number
 if [ $TRAVIS_OS_NAME = linux ]; then
     echo $TRAVIS_BRANCH
-    RELEASE_VERSION="$TRAVIS_BRANCH" | awk -F/ '{print $NF}'
+    RELEASE_VERSION=`echo $TRAVIS_BRANCH | awk -F/ '{print $NF}'`
+    echo $RELEASE_VERSION
     docker build --tag=bluebird .
     docker tag bluebird turinginst/bluebird:$RELEASE_VERSION
     docker images --all
