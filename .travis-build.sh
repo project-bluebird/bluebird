47a2849ded9ce6828120e1cebed3ffe8c40018e9
Tarek Allam <t.allam.jr@gmail.com>
2019-09-03 11:10:28 +0100

    [WIP] Get RELEASE_VERSION and use for docker tag

--- .travis-build.sh
+++ .travis-build.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 set -ev
 cd $BLUEBIRD_HOME
-# echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 # If CI on master branch then tag with latest
 if [ $TRAVIS_BRANCH = "master" ]; then
     docker build --tag=bluebird .
-    docker tag twitcher turinginst/bluebird:latest
-    docker push turinginst/bluebird:latest
+    docker tag bluebird turinginst/bluebird:latest
+    # docker push turinginst/bluebird:latest
 fi
 # If CI on release branch branch then tag with that release number
 if [ $TRAVIS_OS_NAME = linux ]; then
     echo $TRAVIS_BRANCH
-    # docker build --tag=bluebird .
-    # docker tag twitcher turinginst/bluebird:1.3.0
+    RELEASE_VERSION="$TRAVIS_BRANCH" | awk -F/ '{print $NF}'
+    docker build --tag=bluebird .
+    docker tag bluebird turinginst/bluebird:$RELEASE_VERSION
+    docker images --all
     # docker push turinginst/bluebird:1.3.0
 fi
