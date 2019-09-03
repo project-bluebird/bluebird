beb52ced7d555421c5fd680d14201729b8e5d25c
Tarek Allam <t.allam.jr@gmail.com>
2019-09-03 12:47:57 +0100

    Run build of release only on certain branch
    REF:
        https://stackoverflow.com/questions/2172352/in-bash-how-can-i-check-if-a-string-begins-with-some-value/2172365
        https://stackoverflow.com/questions/18709962/regex-matching-in-a-bash-if-statement
    
    [SQUASH] Remove tests on issue branches
    
    In prep for upcoming PR

--- .travis-build.sh
+++ .travis-build.sh
@@ -7,6 +7,7 @@ sudo apt -y install docker-ce
 echo '{ "experimental": true }' | sudo tee /etc/docker/daemon.json
 sudo service docker restart
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+# Although there is a warning, the creds are hashed on the travis end first
 cat /home/travis/.docker/config.json
 # If CI on master branch then tag with latest
 if [ $TRAVIS_BRANCH = "master" ]; then
@@ -15,12 +16,11 @@ if [ $TRAVIS_BRANCH = "master" ]; then
     # docker push turinginst/bluebird:latest
 fi
 # If CI on release branch branch then tag with that release number
-if [ $TRAVIS_OS_NAME = linux ]; then
+if [[ $TRAVIS_BRANCH == release* ]]; then
     echo $TRAVIS_BRANCH
     RELEASE_VERSION=`echo $TRAVIS_BRANCH | awk -F/ '{print $NF}'`
     echo $RELEASE_VERSION
     docker build --tag=bluebird .
     docker tag bluebird turinginst/bluebird:$RELEASE_VERSION
-    docker images --all
     # docker push turinginst/bluebird:1.3.0
 fi
