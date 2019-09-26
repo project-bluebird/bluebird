a4d28033c1933aa839a97e3f5264fb59e1672b2d
Tarek Allam <t.allam.jr@gmail.com>
2019-09-26 14:06:11 +0100

    Move docker commands within conditional block
    It is not required to run the extra docker commands if the branch is not
    on master, this can then be moved to inside the conditional block

--- .travis-build.sh
+++ .travis-build.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 set -ev
-cd $BLUEBIRD_HOME
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt update
-sudo apt -y install docker-ce
-echo '{ "experimental": true }' | sudo tee /etc/docker/daemon.json
-sudo service docker restart
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-# Although there is a warning, the creds are hashed on the travis end first
-cat /home/travis/.docker/config.json
 # If CI on master branch then tag with latest
 if [ "$TRAVIS_BRANCH" = "master" ]; then
+
+    cd $BLUEBIRD_HOME
+
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+    sudo apt update
+    sudo apt -y install docker-ce
+    echo '{ "experimental": true }' | sudo tee /etc/docker/daemon.json
+    sudo service docker restart
+    echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+    # Although there is a warning, the creds are hashed on the travis end first
+    cat /home/travis/.docker/config.json
+
     TAG_VERSION=`git describe --tags`
     echo $TAG_VERSION
     docker build --tag=bluebird .
