b9b4114f3a3b0d928f7ca31d86ff02c4ffaa6533
Ruairidh MacLeod <5160559+rkm@users.noreply.github.com>
2019-11-14 12:50:02 +0000

    Add test for DIRECT api

--- bluebird/api/resources/direct.py
+++ bluebird/api/resources/direct.py
@@ -6,7 +6,7 @@ Provides logic for the DIRECT API endpoint
 
 from flask_restful import Resource, reqparse
 
-from bluebird.api.resources.utils.responses import bad_request_resp, checked_resp
+from bluebird.api.resources.utils.responses import bad_request_resp, ok_resp
 import bluebird.api.resources.utils.utils as utils
 from bluebird.utils.types import Callsign
 
@@ -45,4 +45,4 @@ class Direct(Resource):
 
         err = utils.sim_client().aircraft.direct_to_waypoint(callsign, waypoint)
 
-        return checked_resp(err)
+        return bad_request_resp(err) if err else ok_resp()
---
+++ tests/unit/api/direct_test.py
@@ -0,0 +1,68 @@
+"""
+Tests for the DIRECT endpoint
+"""
+
+from http import HTTPStatus
+
+import bluebird.api.resources.utils.utils as utils
+
+from tests.unit import API_PREFIX
+
+
+def test_direct_post(
+    test_flask_client, patch_sim_proxy
+):  # pylint:disable=unused-argument
+    """
+    Tests the POST method
+    """
+
+    endpoint = f"{API_PREFIX}/direct"
+
+    # Test arg parsing
+
+    resp = test_flask_client.post(endpoint)
+    assert resp.status_code == HTTPStatus.BAD_REQUEST
+
+    data = {}
+    resp = test_flask_client.post(endpoint, json=data)
+    assert resp.status_code == HTTPStatus.BAD_REQUEST
+
+    data = {utils.CALLSIGN_LABEL: "T"}
+    resp = test_flask_client.post(endpoint, json=data)
+    assert resp.status_code == HTTPStatus.BAD_REQUEST
+    assert utils.CALLSIGN_LABEL in resp.json["message"]
+
+    data[utils.CALLSIGN_LABEL] = "TEST"
+    resp = test_flask_client.post(endpoint, json=data)
+    assert resp.status_code == HTTPStatus.BAD_REQUEST
+    assert "waypoint" in resp.json["message"]
+
+    # Test waypoint check
+
+    data["waypoint"] = None
+    resp = test_flask_client.post(endpoint, json=data)
+    assert resp.status_code == HTTPStatus.BAD_REQUEST
+    assert resp.data.decode() == "Waypoint name not specified"
+
+    data["waypoint"] = ""
+    resp = test_flask_client.post(endpoint, json=data)
+    assert resp.status_code == HTTPStatus.BAD_REQUEST
+    assert resp.data.decode() == "Waypoint name not specified"
+
+    # Test callsign check
+
+    data = {utils.CALLSIGN_LABEL: "XXX", "waypoint": "XXX"}
+    resp = test_flask_client.post(endpoint, json=data)
+    assert resp.status_code == HTTPStatus.BAD_REQUEST
+    assert resp.data.decode() == "Aircraft XXX was not found"
+
+    # Test direct_to_waypoint
+
+    data[utils.CALLSIGN_LABEL] = "TEST"
+    resp = test_flask_client.post(endpoint, json=data)
+    assert resp.status_code == HTTPStatus.BAD_REQUEST
+    assert resp.data.decode() == "Invalid waypoint"
+
+    data["waypoint"] = "FIX"
+    resp = test_flask_client.post(endpoint, json=data)
+    assert resp.status_code == HTTPStatus.OK
