893c92ecdd345d2ae6e856bc38715b8b8abdd56c
Ruairidh MacLeod <5160559+rkm@users.noreply.github.com>
2019-11-14 18:56:00 +0000

    Fix pytest debug output

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -4,8 +4,6 @@ Basic test configuration
 
 import pytest
 
-from bluebird.logging import _LOGGER
-
 
 @pytest.fixture(scope="function", autouse=True)
 def log_break():
@@ -13,5 +11,6 @@ def log_break():
     Adds a line break in the debug log file before each test
     :return:
     """
-
-    _LOGGER.debug(f"\n=== New test ===")
+    print()
+    yield
+    print(f"\n=== New test ===")
