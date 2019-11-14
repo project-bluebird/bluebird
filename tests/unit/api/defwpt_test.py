23e2bb96fbc3b12dffb4ce423e9997fc8fbeb0bb
Ruairidh MacLeod <5160559+rkm@users.noreply.github.com>
2019-11-14 12:50:20 +0000

    Update DEFWPT test

--- tests/unit/api/defwpt_test.py
+++ tests/unit/api/defwpt_test.py
@@ -8,32 +8,41 @@ from tests.unit import API_PREFIX
 from tests.unit.api import TEST_LAT, TEST_LON
 
 
-def test_cre_post(test_flask_client, patch_sim_proxy):  # pylint:disable=unused-argument
+def test_defwpt_post(
+    test_flask_client, patch_sim_proxy
+):  # pylint:disable=unused-argument
     """
     Tests the POST method
     """
 
-    resp = test_flask_client.post(f"{API_PREFIX}/defwpt")
+    # Test arg parsing
+
+    endpoint = f"{API_PREFIX}/defwpt"
+    resp = test_flask_client.post(endpoint)
     assert resp.status_code == HTTPStatus.BAD_REQUEST
 
     data = {"wpname": "", "lat": 91, "lon": TEST_LON}
-    resp = test_flask_client.post(f"{API_PREFIX}/defwpt", json=data)
+    resp = test_flask_client.post(endpoint, json=data)
     assert resp.status_code == HTTPStatus.BAD_REQUEST
     assert resp.data.decode() == "Waypoint name must be provided"
 
+    # Test LatLon created
+
     data["wpname"] = "WPT1"
-    resp = test_flask_client.post(f"{API_PREFIX}/defwpt", json=data)
+    resp = test_flask_client.post(endpoint, json=data)
     assert resp.status_code == HTTPStatus.BAD_REQUEST
     assert resp.data.decode().startswith("Invalid LatLon")
 
+    # Test define_waypoint
+
     data["lat"] = TEST_LAT
-    resp = test_flask_client.post(f"{API_PREFIX}/defwpt", json=data)
+    resp = test_flask_client.post(endpoint, json=data)
     assert resp.status_code == HTTPStatus.CREATED
 
     data["type"] = "XXX"
-    resp = test_flask_client.post(f"{API_PREFIX}/defwpt", json=data)
+    resp = test_flask_client.post(endpoint, json=data)
     assert resp.status_code == HTTPStatus.BAD_REQUEST
 
     data["type"] = "FIX"
-    resp = test_flask_client.post(f"{API_PREFIX}/defwpt", json=data)
+    resp = test_flask_client.post(endpoint, json=data)
     assert resp.status_code == HTTPStatus.CREATED
