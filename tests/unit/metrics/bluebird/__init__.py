f912856b06b1048a40cdf8fcbfab14ab31eb43cc
Ruairidh MacLeod <5160559+rkm@users.noreply.github.com>
2019-12-12 13:12:37 +0000

    Update metrics test package

---
+++ tests/unit/metrics/bluebird/__init__.py
@@ -0,0 +1,3 @@
+"""
+Test package for BlueBird's built-int metrics (provided by Aviary)
+"""
---
+++ tests/unit/metrics/bluebird/metrics_test.py
@@ -0,0 +1,11 @@
+"""
+Tests for BlueBird's built-in metrics (provided by Aviary)
+"""
+
+
+def test_pairwise_separation_metric():
+    pass
+
+
+def test_sector_exit_metric():
+    pass
