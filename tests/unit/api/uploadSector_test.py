5daa7c1319909c11ef3441a1778f44e3cc7dc3c5
radka-j <r.jersakova@gmail.com>
2019-12-18 09:39:04 +0000

    update tests

--- tests/unit/api/uploadSector_test.py
+++ tests/unit/api/uploadSector_test.py
@@ -18,11 +18,25 @@ _ENDPOINT = f"{API_PREFIX}/uploadSector"
 @pytest.fixture
 def _set_bb_app(monkeypatch):
     mock = MockBlueBird()
-    mock.sim_proxy.set_props(None, None, None)
     monkeypatch.setattr(api_utils, "_bb_app", lambda: mock)
 
 
 def test_uploadSector(test_flask_client, _set_bb_app):
+
+    # Test error handling - content not provided
+
+    resp = test_flask_client.post(_ENDPOINT)
+    assert resp.status_code == HTTPStatus.BAD_REQUEST
+
+    resp = test_flask_client.post(_ENDPOINT, json={})
+    assert resp.status_code == HTTPStatus.BAD_REQUEST
+
+    # Test error handling - content did not pass validation check
+
+    resp = test_flask_client.post(_ENDPOINT, json={"content": {}})
+    assert resp.status_code == HTTPStatus.BAD_REQUEST
+
+    # Test CREATED response
 
     geoJSON = {"features": [{"type": "Feature", "geometry": {}, "properties": {"name": "test_sector", "type": "SECTOR", "children": {"SECTOR_VOLUME": {"names": ["221395673130872533"]}, "ROUTE": {"names": ["ASCENSION", "FALLEN"]}}}}, {"type": "Feature", "geometry": {"type": "Polygon", "coordinates": [[[-0.2596527086555938, 51.08375683891335], [-0.26207557205922527, 51.916052359621695], [0.007075572059225247, 51.916052359621695], [0.004652708655593784, 51.08375683891335], [-0.2596527086555938, 51.08375683891335]]]}, "properties": {"name": "221395673130872533", "type": "SECTOR_VOLUME", "lower_limit": 60, "upper_limit": 460, "children": {}}}, {"type": "Feature", "properties": {"name": "ASCENSION", "type": "ROUTE", "children": {"FIX": {"names": ["FIYRE", "EARTH", "WATER", "AIR", "SPIRT"]}}}, "geometry": {"type": "LineString", "coordinates": [[-0.1275, 50.91735552314281], [-0.1275, 51.08383154960228], [-0.1275, 51.49999999999135], [-0.1275, 51.916128869951486], [-0.1275, 52.08256690115545]]}}, {"type": "Feature", "properties": {"name": "FALLEN", "type": "ROUTE", "children": {"FIX": {"names": ["SPIRT", "AIR", "WATER", "EARTH", "FIYRE"]}}}, "geometry": {"type": "LineString", "coordinates": [[-0.1275, 52.08256690115545], [-0.1275, 51.916128869951486], [-0.1275, 51.49999999999135], [-0.1275, 51.08383154960228], [-0.1275, 50.91735552314281]]}}, {"type": "Feature", "properties": {"name": "SPIRT", "type": "FIX"}, "geometry": {"type": "Point", "coordinates": [-0.1275, 52.08256690115545]}}, {"type": "Feature", "properties": {"name": "AIR", "type": "FIX"}, "geometry": {"type": "Point", "coordinates": [-0.1275, 51.916128869951486]}}, {"type": "Feature", "properties": {"name": "WATER", "type": "FIX"}, "geometry": {"type": "Point", "coordinates": [-0.1275, 51.49999999999135]}}, {"type": "Feature", "properties": {"name": "EARTH", "type": "FIX"}, "geometry": {"type": "Point", "coordinates": [-0.1275, 51.08383154960228]}}, {"type": "Feature", "properties": {"name": "FIYRE", "type": "FIX"}, "geometry": {"type": "Point", "coordinates": [-0.1275, 50.91735552314281]}}]}
 
@@ -30,7 +44,3 @@ def test_uploadSector(test_flask_client, _set_bb_app):
 
     resp = test_flask_client.post(_ENDPOINT, json=data)
     assert resp.status_code == HTTPStatus.CREATED
-
-
-def test():
-    pytest.xfail("Implement tests, currently just a placeholder")
