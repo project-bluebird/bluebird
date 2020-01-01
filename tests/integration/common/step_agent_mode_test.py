b299e81cae700b7d3c7dd1d63226c5fb8c2ee823
Ruairidh MacLeod <5160559+rkm@users.noreply.github.com>
2020-01-01 22:04:12 +0000

    Fix remaining integration test import

--- tests/integration/common/step_agent_mode_test.py
+++ tests/integration/common/step_agent_mode_test.py
@@ -5,34 +5,34 @@ Tests for the agent mode and the STEP command
 import pytest
 import requests
 
-from tests.integration import API_URL_BASE
+import tests.integration
 
 
 def test_step_agent_mode():
-    """
-    Tests that IC and STEP perform correctly in agent mode with BlueSky
-    """
+    """Tests that IC and STEP perform correctly in agent mode with BlueSky"""
 
     pytest.xfail()
 
-    resp = requests.post(f"{API_URL_BASE}/simmode", json={"mode": "agent"})
+    api_base = tests.integration.API_BASE
+
+    resp = requests.post(f"{api_base}/simmode", json={"mode": "agent"})
     assert resp.status_code == 200, "Expected the mode to be set"
 
-    resp = requests.post(f"{API_URL_BASE}/ic", json={"filename": "8.SCN"})
+    resp = requests.post(f"{api_base}/ic", json={"filename": "8.SCN"})
     assert resp.status_code == 200, "Expected the scenario to be loaded"
 
-    resp = requests.get(f"{API_URL_BASE}/pos?acid=SCN1001")
+    resp = requests.get(f"{api_base}/pos?acid=SCN1001")
     assert resp.status_code == 200, "Expected to get the aircraft position"
 
     initial_sim_t = resp.json()["sim_t"]
 
-    resp = requests.post(f"{API_URL_BASE}/dtmult", json={"multiplier": 20})
+    resp = requests.post(f"{api_base}/dtmult", json={"multiplier": 20})
     assert resp.status_code == 200, "Expected DTMULT to be set"
 
-    resp = requests.post(f"{API_URL_BASE}/step")
+    resp = requests.post(f"{api_base}/step")
     assert resp.status_code == 200, "Expected the simulation was stepped"
 
-    resp = requests.get(f"{API_URL_BASE}/pos?acid=SCN1001")
+    resp = requests.get(f"{api_base}/pos?acid=SCN1001")
     assert resp.status_code == 200, "Expected to get the aircraft position"
 
     diff = resp.json()["sim_t"] - initial_sim_t
