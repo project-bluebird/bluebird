56e58f9704d27232fe6801de3c7189071a371c61
Ruairidh MacLeod <5160559+rkm@users.noreply.github.com>
2020-01-01 19:51:46 +0000

    Restrict valid --integration-sim options

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,4 +9,9 @@ def pytest_addoption(parser):
     # TODO(RKM 2019-12-31) Update docs
     parser.addoption("--run-integration", action="store_true")
     parser.addoption("--docker-host", action="store", default="localhost")
-    parser.addoption("--integration-sim", action="store", default="bluesky")
+    parser.addoption(
+        "--integration-sim",
+        action="store",
+        default="bluesky",
+        choices=["bluesky", "machcoll"],
+    )
