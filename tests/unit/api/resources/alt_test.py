9821bf3019bc196f9c8af398a2fda963f1bab8c1
Ruairidh MacLeod <5160559+rkm@users.noreply.github.com>
2020-01-18 14:35:22 +0000

    Remove test for alt GET method

--- tests/unit/api/resources/alt_test.py
+++ tests/unit/api/resources/alt_test.py
@@ -6,13 +6,10 @@ from http import HTTPStatus
 
 import mock
 
-import bluebird.api as api
 import bluebird.api.resources.utils.utils as utils
-from bluebird.api.resources.utils.responses import bad_request_resp
 
 from tests.unit.api.resources import endpoint_path
 from tests.unit.api.resources import patch_utils_path
-from tests.unit.api.resources import TEST_AIRCRAFT_PROPS
 
 
 _ENDPOINT = "alt"
@@ -67,59 +64,3 @@ def test_alt_post(test_flask_client):
 
         resp = test_flask_client.post(_ENDPOINT_PATH, json=data)
         assert resp.status_code == HTTPStatus.OK
-
-
-def test_alt_get(test_flask_client):
-    """Tests the GET method"""
-
-    # Test arg parsing
-
-    resp = test_flask_client.get(f"{_ENDPOINT_PATH}?")
-    assert resp.status_code == HTTPStatus.BAD_REQUEST
-
-    resp = test_flask_client.get(f"{_ENDPOINT_PATH}?{utils.CALLSIGN_LABEL}=T")
-    assert resp.status_code == HTTPStatus.BAD_REQUEST
-    assert utils.CALLSIGN_LABEL in resp.json["message"]
-
-    with mock.patch(patch_utils_path(_ENDPOINT), wraps=utils) as utils_patch:
-
-        mock_sim_proxy = mock.Mock()
-        utils_patch.sim_proxy.return_value = mock_sim_proxy
-        utils_patch.CALLSIGN_LABEL = utils.CALLSIGN_LABEL
-
-        # Test aircraft exists check
-
-        with api.FLASK_APP.test_request_context():
-            utils_patch.check_exists.return_value = bad_request_resp("Missing aircraft")
-
-        api_path = (
-            f"{_ENDPOINT_PATH}?{utils.CALLSIGN_LABEL}={TEST_AIRCRAFT_PROPS.callsign}"
-        )
-        resp = test_flask_client.get(api_path)
-        assert resp.status_code == HTTPStatus.BAD_REQUEST
-        assert resp.data.decode() == "Missing aircraft"
-
-        # Test error response from get_properties
-
-        utils_patch.check_exists.return_value = None
-        mock_sim_proxy.aircraft.get_properties.return_value = "Error"
-
-        resp = test_flask_client.get(api_path)
-        assert resp.status_code == HTTPStatus.INTERNAL_SERVER_ERROR
-        assert (
-            resp.data.decode()
-            == f"Couldn't get properties for {TEST_AIRCRAFT_PROPS.callsign}: Error"
-        )
-
-        # Test valid response
-
-        mock_sim_proxy.aircraft.get_properties.return_value = TEST_AIRCRAFT_PROPS
-
-        resp = test_flask_client.get(api_path)
-        assert resp.status_code == HTTPStatus.OK
-        assert TEST_AIRCRAFT_PROPS.callsign in resp.json
-
-        props = resp.json[TEST_AIRCRAFT_PROPS.callsign]
-        assert props["fl_current"] == TEST_AIRCRAFT_PROPS.altitude.feet
-        assert props["fl_cleared"] == TEST_AIRCRAFT_PROPS.cleared_flight_level.feet
-        assert props["fl_requested"] == TEST_AIRCRAFT_PROPS.requested_flight_level.feet
