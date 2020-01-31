6f2f353c53225025fe012efcd32371ebc500cb4a
Ruairidh MacLeod <5160559+rkm@users.noreply.github.com>
2020-01-31 00:44:34 +0000

    Simplify test output when mc_client missing

--- tests/unit/sim_client/machcoll/sim_client_test.py
+++ tests/unit/sim_client/machcoll/sim_client_test.py
@@ -9,9 +9,19 @@ _MODULE_NAME = "MachColl"
 
 def test_sim_client_module_import():
     """Test that the module can be imported without error"""
-    sim_client_module_import(_MODULE_NAME)
+    failed = False
+    try:
+        sim_client_module_import(_MODULE_NAME)
+    except ModuleNotFoundError:
+        failed = True
+    assert not failed
 
 
 def test_sim_client_instantiation():
     """Tests that the SimClient can be instantiated"""
-    sim_client_instantiation(_MODULE_NAME)
+    failed = False
+    try:
+        sim_client_instantiation(_MODULE_NAME)
+    except ModuleNotFoundError:
+        failed = True
+    assert not failed
